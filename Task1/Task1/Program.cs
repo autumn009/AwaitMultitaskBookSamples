b3795e27e6d911972f1264ac1cdd7c6413204776
Akira Kawamata <autumn@piedey.co.jp>
2022-12-04 15:33:37 +0900

    add Task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿await Task.Run(async () => {
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write($"{i} ");
+        await Task.Delay(100);
+    }
+});
+Console.WriteLine("Done");
---
+++ Thread1/Thread1/Program.cs
@@ -0,0 +1,10 @@
+﻿var thread = new Thread(() => {
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write($"{i} ");
+        Thread.Sleep(100);
+    }
+});
+thread.Start();
+thread.Join();
+Console.WriteLine("Done");
