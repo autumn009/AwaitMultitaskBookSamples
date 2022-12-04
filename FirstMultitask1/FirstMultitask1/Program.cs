83787d43a7b0867729b974f9ec0dda0187eb409d
Akira Kawamata <autumn@piedey.co.jp>
2022-12-04 11:58:38 +0900

    add FirstMultitask1

---
+++ FirstMultitask1/FirstMultitask1/Program.cs
@@ -0,0 +1,16 @@
+﻿_ = Task.Run(async () =>
+{
+    Console.WriteLine($"Sub Thread ID={Thread.CurrentThread.ManagedThreadId}");
+    foreach (var item in Enumerable.Range('0', 10))
+    {
+        Console.Write((char)item);
+        await Task.Delay(300);
+    }
+});
+
+Console.WriteLine($"Main Thread ID={Thread.CurrentThread.ManagedThreadId}");
+foreach (var item in Enumerable.Range('A', 26))
+{
+    Console.Write((char)item);
+    await Task.Delay(100);
+}
