8596420f294fad6a5c98768403e4d630e00d2033
Akira Kawamata <autumn@piedey.co.jp>
2022-12-06 16:36:28 +0900

    add Intro1

---
+++ Intro1/Intro1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Type Enter to Exit");
+subA();
+subB();
+await Console.In.ReadLineAsync();
+
+async void subA()
+{
+    Console.Write("[Starting subA]");
+    foreach (var item in Enumerable.Range('A',5))
+    {
+        Console.Write((char)item);
+        await Task.Delay(200);
+    }
+    Console.Write("[Done subA]");
+}
+
+async void subB()
+{
+    Console.Write("[Starting subB]");
+    foreach (var item in Enumerable.Range('a', 5))
+    {
+        Console.Write((char)item);
+        await Task.Delay(300);
+    }
+    Console.Write("[Done subB]");
+}
