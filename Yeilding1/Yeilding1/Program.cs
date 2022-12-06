b3ab91f456d623a820bf1fe8cfcf1312935be0ef
Akira Kawamata <autumn@piedey.co.jp>
2022-12-06 18:19:54 +0900

    add Yielding1

---
+++ Yeilding1/Yeilding1/Program.cs
@@ -0,0 +1,19 @@
+﻿subB();
+await subA();
+
+async Task subA()
+{
+    await Console.In.ReadLineAsync();
+}
+
+async void subB()
+{
+    await Console.Out.WriteLineAsync("Start subB");
+    int x = 0;
+    for (int i = 0; i < int.MaxValue; i++)
+    {
+        x += Random.Shared.Next();
+        if (x % 100 == 0) await Task.Yield();
+    }
+    Console.WriteLine(x);
+}
