35481b7fa415bafb06b099d68f2a97878bddd670
Akira Kawamata <autumn@piedey.co.jp>
2022-12-06 14:52:24 +0900

    add Wpf2

---
+++ Wpf2/Wpf1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var tid = Thread.CurrentThread.ManagedThreadId;
+            var taskid = Task.CurrentId;
+            reportIt("Load", tid, taskid);
+            subAsync();
+        }
+
+        private async void subAsync()
+        {
+            var tid = Thread.CurrentThread.ManagedThreadId;
+            var taskid = Task.CurrentId;
+            for (int i = 0; i < 10; i++)
+            {
+                reportIt(i.ToString(), tid, taskid);
+                await Task.Delay(1000);
+            }
+        }
+
+        private void reportIt(string s, int tid, int? taskid)
+        {
+            var t = "null";
+            if (taskid != null) t = taskid.ToString();
+            textBox1.Text += $"{s}: Thread ID:{tid} Task ID:{t}\r\n";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tid = Thread.CurrentThread.ManagedThreadId;
+            var taskid = Task.CurrentId;
+            reportIt("Button", tid, taskid);
+        }
+    }
+}
